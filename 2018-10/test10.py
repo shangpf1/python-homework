48932d6fc1c03ef85d5e3435612c9a4a8c1b6c2a
shangpf1 <208418427@qq.com>
2018-11-04 18:32:46 +0800

    字典的魔法1

--- 2018-10/test10.py
+++ 2018-10/test10.py
@@ -14,3 +14,27 @@ for item in dict:
 
 for item in dict:
     print(item,dict[item])
+
+# 根据序列创建字典，并指定统一的值
+v = dict.fromkeys(['234','frt'],123)
+print(v)
+
+
+# 根据key获取值，key不存在时，可以指定默认值
+v1 = dict.get('k1')
+print(v1)
+
+v2 = dict.get('k2',344)
+print(v2)
+
+dict1 = {6:'123','age':12,'class':6}
+v3 = dict1.pop('class')
+print(v3)
+
+# 更新的2中方法
+dict1.update({'age':90,'sex':'男'})
+print(dict1)
+
+dict1.update(age='70',name='lucy')
+print(dict1)
+
