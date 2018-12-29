09c0dcd95439ae4d345abc5b81ffb64abf9f793e
shangpf1 <208418427@qq.com>
2018-12-29 23:34:25 +0800

    图片上传及截图操作

---
+++ 2019-01/test2.py
@@ -0,0 +1,19 @@
+# 图片的上传及截图
+
+from selenium import webdriver
+from os import path
+
+
+d = path.dirname(__file__)
+index = path.join(d,'index.png')
+image = path.join(d,'image.png')
+driver = webdriver.Chrome()
+
+driver.get("https://www.baidu.com/")
+driver.save_screenshot('./index.png')
+
+driver.find_element_by_class_name('soutu-btn').click()
+driver.save_screenshot('./image.png')
+
+# 点击本地上传按钮，后面的路径是windows中的路径格式
+driver.find_element_by_class_name('upload-pic').send_keys('C:\\Users\\shangpengfei\\Desktop\\Penguins.jpg')
