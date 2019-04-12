84a19c21f1e9e9e77a72f8bc7939d9f0e86325de
shangpf1 <208418427@qq.com>
2019-04-12 11:13:24 +0800

    map、reduce、filter函数的总结实践

--- 2018-12/test9.py
+++ 2018-12/test9.py
@@ -1,14 +1,17 @@
+# map、reduce、filter函数的总结
 
 l = [1,2,4,6]
-print(list(map(str,l)))
+print(list(map(str,l))) 
+# 运行结果：['1', '2', '4', '6']
 
 
 from functools import reduce
 res = reduce(lambda x,y:x+y,l,3)
 print(res)
+# 运行结果：16
 
 name = ['alex_sb','lifei']
-
 resp = filter(lambda x:x.endswith('sb'),name)
 print(list(resp))
+# 运行结果：['alex_sb']
 
