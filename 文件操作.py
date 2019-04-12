bd64e7121acc6ae2e1ddb2a57c1961ea8491dd75
shangpf1 <208418427@qq.com>
2019-04-12 11:56:49 +0800

    文件的打开读取及关闭

--- 文件操作.py
+++ 文件操作.py
@@ -1,7 +1,7 @@
 # 文件处理的3步操作（打开、读取及关闭）
 # 切记：下面的慕容飞儿文件必须与此文件在同级目录下
 
-
+# 打开文件
 f = open('慕容飞儿',encoding='utf-8')
 # data = f.read()
 # print(data)
@@ -15,7 +15,6 @@ f = open('慕容飞儿',encoding='utf-8')
 # print('第三次打印：',data)
 
 # readlines用法
-
 data = f.readlines()
 print('第一次打印：',data)
 print('第二次打印：',data)
